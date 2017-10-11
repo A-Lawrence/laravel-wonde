73a7f0bbc58a6af2b6f2037b40093c6515b0f990
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2017-10-11 16:11:10 +0100

    Basic structure setup.

---
+++ config/wonde.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of Laravel Wonde.
+ *
+ * (c) Anthony Lawrence <artisan@anthonylawrence.me.uk>
+ *
+ * Based on the Laravel Manager package by Graham Campbell.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * which was distributed with this source code.
+ *
+ */
